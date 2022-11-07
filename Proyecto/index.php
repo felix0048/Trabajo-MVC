6fff03f86b793e17c0c67bebac53e22c825a9759
felix0048 <100976694+felix0048@users.noreply.github.com>
2022-11-06 21:36:23 -0500

    Add files via upload

---
+++ Proyecto/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once("c://xampp/htdocs/proyecto/view/head/head.php");
+
+?>
+
+
+<a href="/proyecto/view/username/create.php" class="btn btn-primary">Agregar Nuevo Usuario</a>
+
+
+
+
+
+
+<?php
+require_once("c://xampp/htdocs/proyecto/view/head/footer.php");
+
+?>
+
---
+++ Proyecto/view/username/store.php
@@ -0,0 +1,10 @@
+<?php
+require_once("c://xampp/htdocs/Proyecto/controller/usernameController.php");
+$obj = new usernameController();
+$obj->guardar($_POST['nombre']);
+
+
+
+
+?>
+
